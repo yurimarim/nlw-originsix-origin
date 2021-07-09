8fdb98b1e02983d56ff55d37e1ee02c586135260
yurimarim <yurimarim@hotmail.com>
2021-07-09 16:53:18 -0300

    Aula 03 - Finalizada - Utilizando Swiper e ScrollReveal

--- main.js
+++ main.js
@@ -32,3 +32,31 @@ window.addEventListener('scroll', () => {
     header.classList.remove('scroll')
   }
 })
+
+/* TESTIMONIALS CAROUSEL - (slider, swiper) */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* SCROLL REVEAL - mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
